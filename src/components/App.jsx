d92f22c08f7e4802638ac6399b2d87f6c80ee1a2
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-10 11:17:31 +0200

    updated files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+  static defaultProps = {
+    step: 1,
+  };
+
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>0</span>
+        <button type="button">Increment by {step}</button>
+        <button type="button">Decrement by {step}</button>
+      </div>
+    );
+  }
+}
